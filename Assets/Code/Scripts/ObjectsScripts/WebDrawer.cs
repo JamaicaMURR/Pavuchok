e9da65479a0a84c663fcf299bed993eae76a2ef7
murr123yandex.ru <murr123yandex.ru@outlook.com>
2021-06-11 13:55:51 +0300

    Increased speeds of pulling & releasing of web

--- Assets/Code/Scripts/ObjectsScripts/WebDrawer.cs
+++ Assets/Code/Scripts/ObjectsScripts/WebDrawer.cs
@@ -6,11 +6,14 @@ public class WebDrawer : MonoBehaviour
 {
     LineRenderer lineRenderer;
 
+    Vector3 shift;
+
     //==================================================================================================================================================================
     public WebProducer webProducer;
 
     public float webWidth = 0.1f;
     public float zShift = 0.5f;
+
     //==================================================================================================================================================================
     private void Awake()
     {
@@ -18,20 +21,23 @@ public class WebDrawer : MonoBehaviour
 
         lineRenderer.startWidth = webWidth;
         lineRenderer.endWidth = webWidth;
+
+        shift = new Vector3(0, 0, zShift);
     }
 
     private void Update()
     {
-        Vector3[] positions = new Vector3[webProducer.knots.Count];
-        Vector3 shift = new Vector3(0, 0, zShift);
+        Vector3[] positions = new Vector3[webProducer.knots.Count + 1];
 
         int i = 0;
 
-        foreach(GameObject knot in webProducer.knots)
+        foreach(WebKnot knot in webProducer.knots)
         {
             positions[i] = knot.transform.position + shift;
             i++;
         }
+
+        positions[i] = webProducer.transform.position + shift;
 
         lineRenderer.positionCount = positions.Length;
         lineRenderer.SetPositions(positions);
